52659e60a09f3ac79188ab25e05f2b13882dd8f5
Aheri Stanford-Asiyo <aherisan@gmail.com>
2015-09-28 02:43:23 -0700

    Solve numbers, nulls, booleans, and strings

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,40 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  // create an empty string and name it storage.
+  console.log('obj we are passing into the stringifyJSON function is:', obj );
+  var storage = '';
+  // first test checks to see whether the obj is a number.  (Base case)  
+  if ( typeof(obj) === 'number' ) {
+    var result = storage + obj;
+    return result;
+  } else if (obj === null) { //test for null
+      return storage + 'null';
+  } else if ( typeof(obj) === 'boolean' ){ //address test for boolean
+      return storage + obj;
+  } else if ( typeof(obj) === 'string') {  //address test for 
+      return storage + '\"' + obj + '\"';
+  } else if (Array.isArray(obj)) {
+    if (obj[0] === undefined) {
+      console.log('this thing, ', obj, 'is an empty array'); 
+      return '['+']';
+    } else { 
+      var newArray = _.map(obj, function(val, index, obj){
+        val = stringifyJSON(val);
+        console.log('the modified array content is ,',val)
+        console.log('array obj to now be passed is:', obj);
+        return obj;})  
+      console.log ('now, when I use my stringifyJSON I get the final array of:', '['+newArray.toString()+']')
+      return '['+'\"' + obj.toString()+ '\"'+']';
+    }
+  } else {return };
 };
+
+
+ 
+
+//Notes: 
+//string coersion seems to work for numbers, functions, booleans, undefined, NaN
+//I can't string-coerse an object.
+//When chained with other items, the null value disappears. 
+//Consider that in sum all values are functions, strings, numbers, booleans, undefined values (like null, undefined and NaN), objects or arrays.
