c19ddb09c553bb277fe8faee3a67402eb0c47416
Aheri Stanford-Asiyo <aherisan@gmail.com>
2015-09-29 04:52:21 -0700

    Solved all stringifiable values w/ recursion

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -2,66 +2,54 @@
 // var stringifyJSON = JSON.stringify;
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
-  // your code goes here
-  // create an empty string and name it storage.
-  console.log('obj we are passing into the stringifyJSON function is:', obj);
-  var storage = '';
-  
+
+  var coerceString = '';
+
   if ((typeof(obj) === 'number') || (obj === null) || (typeof(obj) === 'boolean')) {
-    var result = storage + obj;
+    var result = coerceString + obj;
     return result;
 
-  } else if ((typeof(obj) === 'function') || (obj === undefined)) { 
-    return undefined; //JSON.stringify returns undefined for functions and undefined - priority to address early in the code.
+  } else if ((typeof(obj) === 'function') || (obj === undefined) || (obj === 'undefined' || obj === 'functions')) {
+    return coerceString;
 
-  } else if (typeof(obj) === 'string') { 
-    return storage + '\"' + obj + '\"';
+  } else if (typeof(obj) === 'string') {
+    return coerceString + '\"' + obj + '\"';
 
   } else if (Array.isArray(obj)) {
+    
     if (obj[0] === undefined) {
-      console.log(obj, 'is an empty array');
+      //console.log(obj, 'is an empty array');
       return '[' + ']';
 
     } else if ((typeof(obj[0]) === 'number') && (obj.length === 1)) {
-      console.log(obj, 'is an array with a number in it');
+      //console.log(obj, 'is an array with a number in it');
       return '[' + obj[0].toString() + ']';
 
     } else if ((typeof(obj[0]) === 'string') && (obj.length === 1)) {
-      console.log(obj, 'is an array with a string in it');
+      //console.log(obj, 'is an array with a string in it');
       return '[' + '\"' + obj[0].toString() + '\"' + ']';
 
     } else if ((typeof(obj[0]) === 'number') && (typeof(obj[1]) === 'string')) {
-      //console.log(obj, 'is an array with a number and string in it');
       return '[' + stringifyJSON(obj[0]) + ',' + stringifyJSON(obj[1]) + ']';
+      
     } else {
       var resultsArray = [];
       _.each(obj, function(val, index, obj) {
         var valCopy = _.identity(val)
-        valCopy = stringifyJSON(val); //successfully uses recursion
-        //console.log('the modified array content is ', valCopy)
+        valCopy = stringifyJSON(val); 
         resultsArray.push(valCopy);
       })
       return '[' + resultsArray.join() + ']';
     }
   } else {
-      var resultsArray2 = [];
-      _.each(obj, function(val, key, obj) {
-        var valCopy2 = _.identity(val)
-        valCopy2 = stringifyJSON(val); 
-        var keyCopy = _.identity(key);
-        keyCopy = stringifyJSON(key);
-        resultsArray2.push(keyCopy+':'+valCopy2);
-      })
-      return '{' + resultsArray2.join() + '}';
+    var resultsArray2 = [];
+    _.each(obj, function(val, key, obj) {
+      var valCopy2 = _.identity(val)
+      valCopy2 = stringifyJSON(val);
+      var keyCopy = _.identity(key);
+      keyCopy = stringifyJSON(key);
+      resultsArray2.push(keyCopy + ':' + valCopy2);
+    })
+    return '{' + resultsArray2.join() + '}';
   };
 };
-
-
-
-
-//Notes: 
-//string coersion seems to work for numbers, functions, booleans, undefined, NaN
-//I can't string-coerse an object.
-//When chained with other items, the null value disappears - this is a special case. 
-//Consider that in sum all values are functions, strings, numbers, booleans, undefined values (like null, undefined and NaN), objects or arrays.
-//Functions and undefined should be excluded per documentation
