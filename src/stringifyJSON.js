e22f25b87844bcf0af1e8c0c0d9cc172685daa0d
Aheri Stanford-Asiyo <aherisan@gmail.com>
2015-09-28 23:58:27 -0700

    Solved recursively for all arrays

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,39 +4,40 @@
 var stringifyJSON = function(obj) {
   // your code goes here
   // create an empty string and name it storage.
-  console.log('obj we are passing into the stringifyJSON function is:', obj);
+  // console.log('obj we are passing into the stringifyJSON function is:', obj);
   var storage = '';
-  // first test checks to see whether the obj is a number.  (Base case)  
+  
   if ((typeof(obj) === 'number') || (obj === null) || (typeof(obj) === 'boolean')) {
     var result = storage + obj;
     return result;
 
-  } else if (typeof(obj) === 'string') { //address test for 
+  } else if (typeof(obj) === 'string') { 
     return storage + '\"' + obj + '\"';
 
   } else if (Array.isArray(obj)) {
     if (obj[0] === undefined) {
-      console.log(obj, 'is an empty array');
+      //console.log(obj, 'is an empty array');
       return '[' + ']';
 
     } else if ((typeof(obj[0]) === 'number') && (obj.length === 1)) {
-      console.log(obj, 'is an array with a number in it');
+      //console.log(obj, 'is an array with a number in it');
       return '[' + obj[0].toString() + ']';
 
     } else if ((typeof(obj[0]) === 'string') && (obj.length === 1)) {
-      console.log(obj, 'is an array with a string in it');
+      //console.log(obj, 'is an array with a string in it');
       return '[' + '\"' + obj[0].toString() + '\"' + ']';
     } else if ((typeof(obj[0]) === 'number') && (typeof(obj[1]) === 'string')) {
-      console.log(obj, 'is an array with a number and string in it');
+      //console.log(obj, 'is an array with a number and string in it');
       return '[' + stringifyJSON(obj[0]) + ',' + stringifyJSON(obj[1]) + ']';
     } else {
+      var resultsArray = [];
       _.each(obj, function(val, index, obj) {
         var valCopy = _.identity(val)
         valCopy = stringifyJSON(val); //successfully uses recursion
-        console.log('the modified array content is ', valCopy)
-        return obj;
+        //console.log('the modified array content is ', valCopy)
+        resultsArray.push(valCopy);
       })
-      return '[' + obj.toString() + ']';
+      return '[' + resultsArray.join() + ']';
     }
   } else {
     return
