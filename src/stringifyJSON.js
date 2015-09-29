213c1e693c50c3c5e8ed837c7c0c1b3222e98270
Aheri Stanford-Asiyo <aherisan@gmail.com>
2015-09-28 23:45:01 -0700

    Standardize style

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,45 +1,46 @@
 // this is what you would do if you liked things to be easy:
 // var stringifyJSON = JSON.stringify;
-
 // but you don't so you're going to write it from scratch:
-
 var stringifyJSON = function(obj) {
   // your code goes here
   // create an empty string and name it storage.
-  console.log('obj we are passing into the stringifyJSON function is:', obj );
+  console.log('obj we are passing into the stringifyJSON function is:', obj);
   var storage = '';
   // first test checks to see whether the obj is a number.  (Base case)  
-  if ( ( typeof(obj) === 'number' ) || (obj === null ) || ( typeof(obj) === 'boolean' ) ) {
+  if ((typeof(obj) === 'number') || (obj === null) || (typeof(obj) === 'boolean')) {
     var result = storage + obj;
     return result;
 
-  } else if ( typeof(obj) === 'string') {  //address test for 
+  } else if (typeof(obj) === 'string') { //address test for 
     return storage + '\"' + obj + '\"';
 
   } else if (Array.isArray(obj)) {
     if (obj[0] === undefined) {
-      console.log(obj, 'is an empty array'); 
-      return '['+']';
+      console.log(obj, 'is an empty array');
+      return '[' + ']';
 
-    } else if ( (typeof(obj[0]) === 'number') && (obj.length === 1) ) {
-      console.log(obj, 'is an array with a number in it'); 
-      return '['+ obj[0].toString() +']';
+    } else if ((typeof(obj[0]) === 'number') && (obj.length === 1)) {
+      console.log(obj, 'is an array with a number in it');
+      return '[' + obj[0].toString() + ']';
 
-    } else if ( (typeof(obj[0]) === 'string') && (obj.length === 1) ) {
-      console.log(obj, 'is an array with a string in it'); 
-      return '['+ '\"' + obj[0].toString()+ '\"' +']';
-    } else if ( (typeof(obj[0]) === 'number') && (typeof(obj[1]) === 'string' ) ) {
-      console.log(obj, 'is an array with a number and string in it'); 
-      return '['+ stringifyJSON(obj[0]) + ','+ stringifyJSON(obj[1]) + ']';
-    } else { 
-      _.each(obj, function(val, index, obj){
-        var valCopy = _.identity(val) 
+    } else if ((typeof(obj[0]) === 'string') && (obj.length === 1)) {
+      console.log(obj, 'is an array with a string in it');
+      return '[' + '\"' + obj[0].toString() + '\"' + ']';
+    } else if ((typeof(obj[0]) === 'number') && (typeof(obj[1]) === 'string')) {
+      console.log(obj, 'is an array with a number and string in it');
+      return '[' + stringifyJSON(obj[0]) + ',' + stringifyJSON(obj[1]) + ']';
+    } else {
+      _.each(obj, function(val, index, obj) {
+        var valCopy = _.identity(val)
         valCopy = stringifyJSON(val); //successfully uses recursion
-        console.log('the modified array content is ',valCopy)
-        return obj;})  
-      return '['+ obj.toString()+ ']';
+        console.log('the modified array content is ', valCopy)
+        return obj;
+      })
+      return '[' + obj.toString() + ']';
     }
-  } else {return };
+  } else {
+    return
+  };
 };
 
 
