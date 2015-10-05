229513fbc404b39995a72dc1e8ffd93ce184fe52
Aheri Stanford-Asiyo <aherisan@gmail.com>
2015-10-05 11:44:39 -0700

    Code Cleanup
    Removed console.logs and extraneous notes

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -2,32 +2,21 @@
 // var getElementsByClassName = function (className) {
 //   return document.getElementsByClassName(className);
 // };
-
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className){
+var getElementsByClassName = function(className) {
+  
+  var target = className;
+  var arrayLikeNodeObject = document.body.childNodes;
+  var resultArray = [];
 
-var target = className;
-//console.log(target); //targetClassName - successful.
+  var objTraverser = function(val, index, obj) {
+    if ((val.classList !== undefined) && (target === val.classList[0])) { //look for matches
+      resultArray.push(val);
+    }
+  };
 
-var arrayLikeNodeObject = document.body.childNodes;
-//console.log(arrayLikeNodeObject); // logs 13 correct nodes.
+  _.each(arrayLikeNodeObject, objTraverser); 
 
-var resultArray = [];
-
-var objTraverser = function (val, index, obj) {
-  //console.log(val.classList, index, obj);
-  //console.log('target is ', target, 'and val.classList is ', val.classList);
-  if ( (val.classList !== undefined) && (target === val.classList[0]) ) {
-    //console.log(val.classlist, 'is a match');
-    resultArray.push(val);
-  }
-};
-
-_.each(arrayLikeNodeObject, objTraverser); //use filter to return object into the result array.
-console.log(resultArray);
-return resultArray;
+  return resultArray;
 
 };
-// Note: 
-// DOM contains many many node objects embedded within one another and also note over 200 properties in each one
-// every element is an object, but developer assumes that for this exercise they cannot be stringified and searched
